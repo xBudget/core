d258d0bf6d39fa97be47a1753da3871f3ec8d908
Thiago Silva <thiagodd.silva@gmail.com>
2021-01-24 13:40:01 -0300

    fix ocelot redirect and add health controller

---
+++ src/Services/xBudget.Account.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xBudget.Account.Api.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok($"{ System.Reflection.Assembly.GetEntryAssembly().GetName().Name } is running");
+        }
+    }
+}
---
+++ src/Services/xBudget.Identity.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace xBudget.Identity.Api.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet("status")]
+        public IActionResult Status()
+        {
+            return Ok($"{ System.Reflection.Assembly.GetEntryAssembly().GetName().Name } is running");
+        }
+    }
+}
